377967fdc192c6082c448ae990e6c9dd80efe2c3
jiayi zhan <jiayizhan2005@gmail.com>
2023-05-19 23:20:58 +0200

    selectionsort implemented

--- sorts/selectionsort.js
+++ sorts/selectionsort.js
@@ -1,3 +1,18 @@
 export default function selectionsort(arr) {
+  const a = [...arr];
+  const N = a.length;
+
+  let i, j, mnIndex;
+
+  for (i = 0; i < N - 1; ++i) {
+    mnIndex = i;
+    for (j = i + 1; j < N; ++j) {
+      if (a[j] < a[mnIndex]) {
+        mnIndex = j;
+      }
+    }
+    [a[i], a[mnIndex]] = [a[mnIndex], a[i]];
+  }
+
   return [];
 }
